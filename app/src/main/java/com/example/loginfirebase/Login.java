38b62b9ef2b6c592bdbbbdbdbd7eeed7705550f9
SohamSinghal <sohamsinghal32@gmail.com>
2022-07-22 17:04:25 +0530

    Email Verification

--- app/src/main/java/com/example/loginfirebase/Login.java
+++ app/src/main/java/com/example/loginfirebase/Login.java
@@ -7,7 +7,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,11 +22,12 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
     EditText email,pass;
     Button login,signup;
-    TextView forgetpass;
+    TextView forgetpass,verifyemail;
     FirebaseAuth fauth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +39,7 @@ public class Login extends AppCompatActivity {
         login = findViewById(R.id.login);
         signup = findViewById(R.id.button1);
         forgetpass = findViewById(R.id.ForgotPass);
+        verifyemail = findViewById(R.id.VerifyEmail);
 
         fauth = FirebaseAuth.getInstance();
 
@@ -60,16 +64,43 @@ public class Login extends AppCompatActivity {
                     pass.setError("Enter correct password");
                     return;
                 }
-
                 //Authentication of User
-
                 fauth.signInWithEmailAndPassword(semail,spass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful())
                         {
-                            Toast.makeText(Login.this,"Login Successful",Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                            FirebaseUser user = fauth.getCurrentUser();
+                            Handler handler = new Handler();
+                            handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                }
+                            }, 3000);
+                            if(!user.isEmailVerified()){
+                                Toast.makeText(Login.this, "Email Not Verified.", Toast.LENGTH_SHORT).show();
+                                verifyemail.setVisibility(View.VISIBLE);
+                                verifyemail.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        user.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void unused) {
+                                                Toast.makeText(Login.this, "Email Verification Sent", Toast.LENGTH_SHORT).show();
+                                            }
+                                        }).addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Log.d("tag", "onFailure: Email Not Sent"+e.getMessage());
+                                            }
+                                        });
+                                    }
+                                });
+                            }
+                            else {
+                                Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            }
                         }
                         else
                         {
--- app/src/main/java/com/example/loginfirebase/Signup.java
+++ app/src/main/java/com/example/loginfirebase/Signup.java
@@ -6,20 +6,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Signup extends AppCompatActivity {
     EditText name,email,pass,pass_repeat;
     FirebaseAuth firebaseAuth;
     Button cont;
+    public static final String TAG = "YOUR-TAG-NAME";
 
 
     @Override
@@ -69,7 +74,20 @@ public class Signup extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()) {
-                            Toast.makeText(Signup.this,"Account Created", Toast.LENGTH_SHORT).show();
+                            //Email Verification
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            user.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Toast.makeText(Signup.this, "Email Verification Sent", Toast.LENGTH_SHORT).show();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d(TAG, "onFailure: Email Not Sent"+e.getMessage());
+                                }
+                            });
+                            Toast.makeText(Signup.this,"Account Created. ", Toast.LENGTH_SHORT).show();
                             startActivity(new Intent(getApplicationContext(), Login.class));
                         }
                         else
